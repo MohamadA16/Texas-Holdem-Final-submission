576b78abd5f86b41243a013e94cab6428046956a
Mohamad Alkhaled <malothma@terpmail.umd.edu>
2026-05-04 04:46:15 -0400

    Update test_game.py

--- test_game.py
+++ test_game.py
@@ -7,3 +7,12 @@ def test_deal_hole_cards():
     assert len(game.player1.hand) == 2
     assert len(game.player2.hand) == 2
 
+def test_flop_turn_river_counts():
+    game = Game("A", "B")
+    game.setup_round()
+    game.deal_flop()
+    assert len(game.community_cards) == 3
+    game.deal_turn()
+    assert len(game.community_cards) == 4
+    game.deal_river()
+    assert len(game.community_cards) == 5
