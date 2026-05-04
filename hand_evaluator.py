87c4c4f54941bf86fcc0920922110613b504006b
Mohamad Alkhaled <malothma@terpmail.umd.edu>
2026-05-04 04:56:26 -0400

    Update hand_evaluator.py

--- hand_evaluator.py
+++ hand_evaluator.py
@@ -18,3 +18,28 @@ def evaluate_five_card_hand(cards):
 
     is_flush = len(set(suits)) == 1
     is_straight = False
+    
+    if len(unique_values) == 5:
+        if unique_values[-1] - unique_values[0] == 4:
+            is_straight = True
+        elif unique_values == [2, 3, 4, 5, 14]:
+            is_straight = True
+            values = [5, 4, 3, 2, 1]
+
+    if is_straight and is_flush:
+        return (8, values)
+    if count_values == [4, 1]:
+        return (7, values)
+    if count_values == [3, 2]:
+        return (6, values)
+    if is_flush:
+        return (5, values)
+    if is_straight:
+        return (4, values)
+    if count_values == [3, 1, 1]:
+        return (3, values)
+    if count_values == [2, 2, 1]:
+        return (2, values)
+    if count_values == [2, 1, 1, 1]:
+        return (1, values)
+    return (0, values)
